1c0256c09d35b4c897c181cfa058bc1899688515
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2013-01-21 11:51:50 +0200

    Renamed package

--- src/main/java/com/github/calc/Calc.java
+++ src/main/java/com/github/calc/Calc.java
@@ -1,37 +1,23 @@
 /*
- * Copyright (c) 2010, Oracle. All rights reserved.
+ * $Id$
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Copyright 2013 Valentyn Kolesnikov
  *
- * * Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * * Neither the name of Oracle nor the names of its contributors
- *   may be used to endorse or promote products derived from this software without
- *   specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-package com.github.swingcalculator;
+package com.github.calc;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class Calc extends javax.swing.JFrame {
     private BigDecimal currentValue = BigDecimal.ZERO;
@@ -501,7 +487,7 @@ public class Calc extends javax.swing.JFrame {
                         result = savedValue.multiply(value);
                         break;
                     case '/':
-                        result = savedValue.divide(value, 16, RoundingMode.HALF_UP);
+                        result = savedValue.divide(value, 16, BigDecimal.ROUND_HALF_UP);
                         break;
                 }
                 commandCode = '=';
@@ -558,7 +544,7 @@ public class Calc extends javax.swing.JFrame {
         } else if ("/".equals(command)) {
             if (commandCode != '=' && !initValue) {
                 BigDecimal value = new BigDecimal(jTextField1.getText().replace(',', '.'));
-                BigDecimal result = savedValue.divide(value, 16, RoundingMode.HALF_UP);
+                BigDecimal result = savedValue.divide(value, 16, BigDecimal.ROUND_HALF_UP);
                 jTextField1.setText(result.toString().replace('.', ','));
                 savedValue = result;
                 currentValue = BigDecimal.ZERO;

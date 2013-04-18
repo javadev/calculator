00ca6df325ad9a0d4130d860dac528a679e0edc4
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2013-04-18 11:37:49 +0300

    Improved qunit tests for javascript calculator

--- src/test/js/tests.js
+++ src/test/js/tests.js
@@ -4,6 +4,31 @@ module( "tests", {
     }
 });
 
+test( "add", function() {
+    keyDetect("3");
+    keyDetect(",");
+    keyDetect("1");
+    keyDetect("1");
+    fCalc("-");
+    keyDetect("3");
+    keyDetect(",");
+    keyDetect("1");
+    fCalc("=");
+    equal($id().value, "0,01", "Value must be 0,01, but " + $id().value);
+});
+
+test( "div", function() {
+    keyDetect("1");
+    fCalc("/");
+    keyDetect("7");
+    fCalc("=");
+    equal($id().value, "0,1428571428571429", "Value must be 0,1428571428571429, but " + $id().value);
+    fCalc("*");
+    keyDetect("7");
+    fCalc("=");
+    equal($id().value, "1", "Value must be 1, but " + $id().value);
+});
+
 test( "sqrt", function() {
     keyDetect('9');
     fCalc("sqrt");
@@ -20,5 +45,18 @@ test( "nbs", function() {
     keyDetect('1');
     fCalc("nbs");
     fCalc("=");
-    equal($id().value, "5,1", "Value must be 5.1, but " + $id().value);
+    equal($id().value, "5,1", "Value must be 5,1, but " + $id().value);
+});
+
+test( "comma", function() {
+    keyDetect('3');
+    keyDetect(',');
+    keyDetect('1');
+    fCalc("nbs");
+    fCalc("+");
+    keyDetect('2');
+    keyDetect(',');
+    keyDetect('0');
+    fCalc("=");
+    equal($id().value, "5", "Value must be 5, but " + $id().value);
 });

133fc76f12ec426c9392002681228837b6ed23de
Valentyn Kolesnikov <javadev75@gmail.com>
2023-02-26 22:51:05 +0200

    Updated readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 Calculator
 ==========
 
-[![Maven Central](https://img.shields.io/maven-central/v/com.github.javadev/calc.svg)](http://search.maven.org/#search%7Cga%7C1%7Cg%3A%22com.github.javadev%22%20AND%20a%3A%22calc%22)
+[![Maven Central](https://img.shields.io/maven-central/v/com.github.javadev/calculator?style=flat-square)](https://central.sonatype.com/artifact/com.github.javadev/calculator/1.2)
 [![Java CI with Maven](https://github.com/javadev/calculator/actions/workflows/maven.yml/badge.svg)](https://github.com/javadev/calculator/actions/workflows/maven.yml)
 
 The java/swing, javascript, android calculator
 
-[![Screen short](https://raw.github.com/javadev/calc/master/calc.png)](https://github.com/javadev/calc)
-[![Screen short](https://raw.github.com/javadev/calc/master/calc2.png)](http://javadev.github.io/calc)
+[![Screen short](calc.png)](https://github.com/javadev/calc)
+[![Screen short](calc2.png)](http://javadev.github.io/calc)
--- src/main/java/com/github/calc/BigDecimalUtil.java
+++ src/main/java/com/github/calc/BigDecimalUtil.java
@@ -31,20 +31,21 @@ import java.math.RoundingMode;
 public final class BigDecimalUtil {
     private static final int SCALE = 18;
     public static long ITER = 1000;
-    public static MathContext context = new MathContext( 100 );
+    public static MathContext context = new MathContext(100);
     private static final int ROUNDING_MODE = BigDecimal.ROUND_HALF_EVEN;
-    public static BigDecimal PI_DIV_180
-        = new BigDecimal("3.1415926535897932384626433832795").divide(BigDecimal.valueOf(180), 32, BigDecimal.ROUND_HALF_UP);
-    public static BigDecimal PI_DIV_200
-        = new BigDecimal("3.1415926535897932384626433832795").divide(BigDecimal.valueOf(200), 32, BigDecimal.ROUND_HALF_UP);
+    public static BigDecimal PI_DIV_180 =
+            new BigDecimal("3.1415926535897932384626433832795")
+                    .divide(BigDecimal.valueOf(180), 32, BigDecimal.ROUND_HALF_UP);
+    public static BigDecimal PI_DIV_200 =
+            new BigDecimal("3.1415926535897932384626433832795")
+                    .divide(BigDecimal.valueOf(200), 32, BigDecimal.ROUND_HALF_UP);
     public static BigDecimal EPS = BigDecimal.ONE.scaleByPowerOfTen(-100);
 
-    private BigDecimalUtil() {
-    }
+    private BigDecimalUtil() {}
 
     /**
-     * Compute the square root of x to a given scale, x &gt;= 0.
-     * Use Newton's algorithm.
+     * Compute the square root of x to a given scale, x &gt;= 0. Use Newton's algorithm.
+     *
      * @param x the value of x
      * @return the result value
      */
@@ -77,27 +78,24 @@ public final class BigDecimalUtil {
     }
 
     /**
-     * Compute the integral root of x to a given scale, x &gt;= 0.
-     * Use Newton's algorithm.
+     * Compute the integral root of x to a given scale, x &gt;= 0. Use Newton's algorithm.
+     *
      * @param x the value of x
      * @param index the integral root value
      * @param scale the desired scale of the result
      * @return the result value
      */
-    public static BigDecimal intRoot(BigDecimal x, long index,
-                                     int scale)
-    {
+    public static BigDecimal intRoot(BigDecimal x, long index, int scale) {
         // Check that x >= 0.
         if (x.signum() < 0) {
             throw new IllegalArgumentException("x < 0");
         }
 
-        int        sp1 = scale + 1;
-        BigDecimal n   = x;
-        BigDecimal i   = BigDecimal.valueOf(index);
-        BigDecimal im1 = BigDecimal.valueOf(index-1);
-        BigDecimal tolerance = BigDecimal.valueOf(5)
-                                            .movePointLeft(sp1);
+        int sp1 = scale + 1;
+        BigDecimal n = x;
+        BigDecimal i = BigDecimal.valueOf(index);
+        BigDecimal im1 = BigDecimal.valueOf(index - 1);
+        BigDecimal tolerance = BigDecimal.valueOf(5).movePointLeft(sp1);
         BigDecimal xPrev;
 
         // The initial approximation is x/index.
@@ -107,27 +105,21 @@ public final class BigDecimalUtil {
         // (two successive approximations are equal after rounding).
         do {
             // x^(index-1)
-            BigDecimal xToIm1 = intPower(x, index-1, sp1);
+            BigDecimal xToIm1 = intPower(x, index - 1, sp1);
 
             // x^index
-            BigDecimal xToI =
-                    x.multiply(xToIm1)
-                        .setScale(sp1, BigDecimal.ROUND_HALF_EVEN);
+            BigDecimal xToI = x.multiply(xToIm1).setScale(sp1, BigDecimal.ROUND_HALF_EVEN);
 
             // n + (index-1)*(x^index)
             BigDecimal numerator =
-                    n.add(im1.multiply(xToI))
-                        .setScale(sp1, BigDecimal.ROUND_HALF_EVEN);
+                    n.add(im1.multiply(xToI)).setScale(sp1, BigDecimal.ROUND_HALF_EVEN);
 
             // (index*(x^(index-1))
-            BigDecimal denominator =
-                    i.multiply(xToIm1)
-                        .setScale(sp1, BigDecimal.ROUND_HALF_EVEN);
+            BigDecimal denominator = i.multiply(xToIm1).setScale(sp1, BigDecimal.ROUND_HALF_EVEN);
 
             // x = (n + (index-1)*(x^index)) / (index*(x^(index-1)))
             xPrev = x;
-            x = numerator
-                    .divide(denominator, sp1, BigDecimal.ROUND_DOWN);
+            x = numerator.divide(denominator, sp1, BigDecimal.ROUND_DOWN);
 
             Thread.yield();
         } while (x.subtract(xPrev).abs().compareTo(tolerance) > 0);
@@ -137,6 +129,7 @@ public final class BigDecimalUtil {
 
     /**
      * Compute the natural logarithm of x to a given scale, x &gt; 0.
+     *
      * @param x the value
      * @param scale the scale
      * @return the result
@@ -164,24 +157,20 @@ public final class BigDecimalUtil {
             BigDecimal lnRoot = lnNewton(root, scale);
 
             // magnitude*ln(x^(1/magnitude))
-            return BigDecimal.valueOf(magnitude).multiply(lnRoot)
-                        .setScale(scale, BigDecimal.ROUND_HALF_EVEN);
+            return BigDecimal.valueOf(magnitude)
+                    .multiply(lnRoot)
+                    .setScale(scale, BigDecimal.ROUND_HALF_EVEN);
         }
     }
 
-     /**
-     * Compute the natural logarithm of x to a given scale, x > 0.
-     * Use Newton's algorithm.
-     */
-    private static BigDecimal lnNewton(BigDecimal x, int scale)
-    {
-        int        sp1 = scale + 1;
-        BigDecimal n   = x;
+    /* Compute the natural logarithm of x to a given scale, x > 0. Use Newton's algorithm. */
+    private static BigDecimal lnNewton(BigDecimal x, int scale) {
+        int sp1 = scale + 1;
+        BigDecimal n = x;
         BigDecimal term;
 
         // Convergence tolerance = 5*(10^-(scale+1))
-        BigDecimal tolerance = BigDecimal.valueOf(5)
-                                            .movePointLeft(sp1);
+        BigDecimal tolerance = BigDecimal.valueOf(5).movePointLeft(sp1);
 
         // Loop until the approximations converge
         // (two successive approximations are within the tolerance).
@@ -191,8 +180,7 @@ public final class BigDecimalUtil {
             BigDecimal eToX = exp(x, sp1);
 
             // (e^x - n)/e^x
-            term = eToX.subtract(n)
-                        .divide(eToX, sp1, BigDecimal.ROUND_DOWN);
+            term = eToX.subtract(n).divide(eToX, sp1, BigDecimal.ROUND_DOWN);
 
             // x - (e^x - n)/e^x
             x = x.subtract(term);
@@ -206,11 +194,11 @@ public final class BigDecimalUtil {
     public static BigDecimal cosine(BigDecimal x) {
 
         BigDecimal currentValue = BigDecimal.ONE;
-        BigDecimal lastVal      = currentValue.add(BigDecimal.ONE);
-        BigDecimal xSquared     = x.multiply(x);
-        BigDecimal numerator    = BigDecimal.ONE;
-        BigDecimal denominator  = BigDecimal.ONE;
-        int        i            = 0;
+        BigDecimal lastVal = currentValue.add(BigDecimal.ONE);
+        BigDecimal xSquared = x.multiply(x);
+        BigDecimal numerator = BigDecimal.ONE;
+        BigDecimal denominator = BigDecimal.ONE;
+        int i = 0;
 
         while (lastVal.compareTo(currentValue) != 0) {
             lastVal = currentValue;
@@ -219,7 +207,7 @@ public final class BigDecimalUtil {
 
             denominator = denominator.multiply(BigDecimal.valueOf(z));
             denominator = denominator.multiply(BigDecimal.valueOf(z - 1));
-            numerator   = numerator.multiply(xSquared);
+            numerator = numerator.multiply(xSquared);
 
             BigDecimal term = numerator.divide(denominator, SCALE + 5, ROUNDING_MODE);
 
@@ -235,12 +223,12 @@ public final class BigDecimalUtil {
     }
 
     public static BigDecimal sine(BigDecimal x) {
-        BigDecimal lastVal      = x.add(BigDecimal.ONE);
+        BigDecimal lastVal = x.add(BigDecimal.ONE);
         BigDecimal currentValue = x;
-        BigDecimal xSquared     = x.multiply(x);
-        BigDecimal numerator    = x;
-        BigDecimal denominator  = BigDecimal.ONE;
-        int        i            = 0;
+        BigDecimal xSquared = x.multiply(x);
+        BigDecimal numerator = x;
+        BigDecimal denominator = BigDecimal.ONE;
+        int i = 0;
 
         while (lastVal.compareTo(currentValue) != 0) {
             lastVal = currentValue;
@@ -249,7 +237,7 @@ public final class BigDecimalUtil {
 
             denominator = denominator.multiply(BigDecimal.valueOf(z));
             denominator = denominator.multiply(BigDecimal.valueOf(z - 1));
-            numerator   = numerator.multiply(xSquared);
+            numerator = numerator.multiply(xSquared);
 
             BigDecimal term = numerator.divide(denominator, SCALE + 5, ROUNDING_MODE);
 
@@ -271,35 +259,34 @@ public final class BigDecimalUtil {
 
         return sin.divide(cos, SCALE, BigDecimal.ROUND_HALF_UP);
     }
-    
+
     public static BigDecimal log10(BigDecimal b) {
         final int NUM_OF_DIGITS = SCALE + 2;
-            // need to add one to get the right number of dp
-            //  and then add one again to get the next number
-            //  so I can round it correctly.
-    
+        // need to add one to get the right number of dp
+        //  and then add one again to get the next number
+        //  so I can round it correctly.
+
         MathContext mc = new MathContext(NUM_OF_DIGITS, RoundingMode.HALF_EVEN);
-        //special conditions:
+        // special conditions:
         // log(-x) -> exception
         // log(1) == 0 exactly;
         // log of a number lessthan one = -log(1/x)
-        if(b.signum() <= 0) {
-                throw new ArithmeticException("log of a negative number! (or zero)");
-            }
-        else if(b.compareTo(BigDecimal.ONE) == 0) {
-                return BigDecimal.ZERO;
-            } else if(b.compareTo(BigDecimal.ONE) < 0) {
-                return (log10((BigDecimal.ONE).divide(b,mc))).negate();
-            }
-    
+        if (b.signum() <= 0) {
+            throw new ArithmeticException("log of a negative number! (or zero)");
+        } else if (b.compareTo(BigDecimal.ONE) == 0) {
+            return BigDecimal.ZERO;
+        } else if (b.compareTo(BigDecimal.ONE) < 0) {
+            return (log10((BigDecimal.ONE).divide(b, mc))).negate();
+        }
+
         StringBuilder sb = new StringBuilder();
-        //number of digits on the left of the decimal point
+        // number of digits on the left of the decimal point
         int leftDigits = b.precision() - b.scale();
-    
-        //so, the first digits of the log10 are:
+
+        // so, the first digits of the log10 are:
         sb.append(leftDigits - 1).append(".");
-    
-        //this is the algorithm outlined in the webpage
+
+        // this is the algorithm outlined in the webpage
         int n = 0;
         while (n < NUM_OF_DIGITS) {
             b = (b.movePointLeft(leftDigits - 1)).pow(10, mc);
@@ -307,14 +294,17 @@ public final class BigDecimalUtil {
             sb.append(leftDigits - 1);
             n++;
         }
-    
+
         BigDecimal ans = new BigDecimal(sb.toString());
-    
-        //Round the number to the correct number of decimal places.
-        ans = ans.round(new MathContext(ans.precision() - ans.scale() + SCALE, RoundingMode.HALF_EVEN));
+
+        // Round the number to the correct number of decimal places.
+        ans =
+                ans.round(
+                        new MathContext(
+                                ans.precision() - ans.scale() + SCALE, RoundingMode.HALF_EVEN));
         return ans;
     }
-    
+
     public static BigDecimal cuberoot(BigDecimal b) {
         // Specify a math context with 40 digits of precision.
 
@@ -322,10 +312,16 @@ public final class BigDecimalUtil {
 
         BigDecimal x = new BigDecimal("1", mc);
 
-        // Search for the cube root via the Newton-Raphson loop. Output each // successive iteration's value.
+        // Search for the cube root via the Newton-Raphson loop. Output each // successive
+        // iteration's value.
 
         for (int i = 0; i < ITER; i++) {
-            x = x.subtract(x.pow(3, mc).subtract(b, mc).divide(new BigDecimal("3", mc).multiply(x.pow(2, mc), mc), mc), mc);
+            x =
+                    x.subtract(
+                            x.pow(3, mc)
+                                    .subtract(b, mc)
+                                    .divide(new BigDecimal("3", mc).multiply(x.pow(2, mc), mc), mc),
+                            mc);
         }
         return x;
     }
@@ -337,20 +333,19 @@ public final class BigDecimalUtil {
     }
 
     /**
-     * Compute x^exponent to a given scale.  Uses the same
-     * algorithm as class numbercruncher.mathutils.IntPower.
+     * Compute x^exponent to a given scale. Uses the same algorithm as class
+     * numbercruncher.mathutils.IntPower.
+     *
      * @param x the value x
      * @param exponent the exponent value
      * @param scale the desired scale of the result
      * @return the result value
      */
-    public static BigDecimal intPower(BigDecimal x, long exponent,
-                                      int scale) {
+    public static BigDecimal intPower(BigDecimal x, long exponent, int scale) {
         // If the exponent is negative, compute 1/(x^-exponent).
         if (exponent < 0) {
             return BigDecimal.valueOf(1)
-                        .divide(intPower(x, -exponent, scale), scale,
-                                BigDecimal.ROUND_HALF_EVEN);
+                    .divide(intPower(x, -exponent, scale), scale, BigDecimal.ROUND_HALF_EVEN);
         }
 
         BigDecimal power = BigDecimal.valueOf(1);
@@ -360,13 +355,11 @@ public final class BigDecimalUtil {
 
             // Is the rightmost bit a 1?
             if ((exponent & 1) == 1) {
-                power = power.multiply(x)
-                          .setScale(scale, BigDecimal.ROUND_HALF_EVEN);
+                power = power.multiply(x).setScale(scale, BigDecimal.ROUND_HALF_EVEN);
             }
 
             // Square x and shift exponent 1 bit to the right.
-            x = x.multiply(x)
-                    .setScale(scale, BigDecimal.ROUND_HALF_EVEN);
+            x = x.multiply(x).setScale(scale, BigDecimal.ROUND_HALF_EVEN);
             exponent >>= 1;
 
             Thread.yield();
@@ -376,9 +369,9 @@ public final class BigDecimalUtil {
     }
 
     /**
-     * Compute e^x to a given scale.
-     * Break x into its whole and fraction parts and
-     * compute (e^(1 + fraction/whole))^whole using Taylor's formula.
+     * Compute e^x to a given scale. Break x into its whole and fraction parts and compute (e^(1 +
+     * fraction/whole))^whole using Taylor's formula.
+     *
      * @param x the value of x
      * @param scale the desired scale of the result
      * @return the result value
@@ -392,8 +385,7 @@ public final class BigDecimalUtil {
         // If x is negative, return 1/(e^-x).
         else if (x.signum() == -1) {
             return BigDecimal.valueOf(1)
-                        .divide(exp(x.negate(), scale), scale,
-                                BigDecimal.ROUND_HALF_EVEN);
+                    .divide(exp(x.negate(), scale), scale, BigDecimal.ROUND_HALF_EVEN);
         }
 
         // Compute the whole part of x.
@@ -408,62 +400,58 @@ public final class BigDecimalUtil {
         BigDecimal xFraction = x.subtract(xWhole);
 
         // z = 1 + fraction/whole
-        BigDecimal z = BigDecimal.valueOf(1)
-                            .add(xFraction.divide(
-                                    xWhole, scale,
-                                    BigDecimal.ROUND_HALF_EVEN));
+        BigDecimal z =
+                BigDecimal.valueOf(1)
+                        .add(xFraction.divide(xWhole, scale, BigDecimal.ROUND_HALF_EVEN));
 
         // t = e^z
         BigDecimal t = expTaylor(z, scale);
 
         BigDecimal maxLong = BigDecimal.valueOf(Long.MAX_VALUE);
-        BigDecimal result  = BigDecimal.valueOf(1);
+        BigDecimal result = BigDecimal.valueOf(1);
 
         // Compute and return t^whole using intPower().
         // If whole > Long.MAX_VALUE, then first compute products
         // of e^Long.MAX_VALUE.
         while (xWhole.compareTo(maxLong) >= 0) {
-            result = result.multiply(
-                                intPower(t, Long.MAX_VALUE, scale))
-                        .setScale(scale, BigDecimal.ROUND_HALF_EVEN);
+            result =
+                    result.multiply(intPower(t, Long.MAX_VALUE, scale))
+                            .setScale(scale, BigDecimal.ROUND_HALF_EVEN);
             xWhole = xWhole.subtract(maxLong);
 
             Thread.yield();
         }
         return result.multiply(intPower(t, xWhole.longValue(), scale))
-                        .setScale(scale, BigDecimal.ROUND_HALF_EVEN);
+                .setScale(scale, BigDecimal.ROUND_HALF_EVEN);
     }
 
     /**
      * Compute e^x to a given scale by the Taylor series.
+     *
      * @param x the value of x
      * @param scale the desired scale of the result
      * @return the result value
      */
-    private static BigDecimal expTaylor(BigDecimal x, int scale)
-    {
+    private static BigDecimal expTaylor(BigDecimal x, int scale) {
         BigDecimal factorial = BigDecimal.valueOf(1);
-        BigDecimal xPower    = x;
+        BigDecimal xPower = x;
         BigDecimal sumPrev;
 
         // 1 + x
-        BigDecimal sum  = x.add(BigDecimal.valueOf(1));
+        BigDecimal sum = x.add(BigDecimal.valueOf(1));
 
         // Loop until the sums converge
         // (two successive sums are equal after rounding).
         int i = 2;
         do {
             // x^i
-            xPower = xPower.multiply(x)
-                        .setScale(scale, BigDecimal.ROUND_HALF_EVEN);
+            xPower = xPower.multiply(x).setScale(scale, BigDecimal.ROUND_HALF_EVEN);
 
             // i!
             factorial = factorial.multiply(BigDecimal.valueOf(i));
 
             // x^i/i!
-            BigDecimal term = xPower
-                                .divide(factorial, scale,
-                                        BigDecimal.ROUND_HALF_EVEN);
+            BigDecimal term = xPower.divide(factorial, scale, BigDecimal.ROUND_HALF_EVEN);
 
             // sum = sum + x^i/i!
             sumPrev = sum;
